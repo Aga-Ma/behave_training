f986486a1300f5ef1cab75ec6fec7c769cf782d2
Aga <agnieszka.jureczko.malek@gmail.com>
2020-01-29 11:57:18 +0100

    revert last commit
    This reverts commit d1f214c458c18c31fb7b036ce329cea4edbf0390.

--- features/steps/step_parameters.py
+++
@@ -1,7 +0,0 @@
-from behave import *
-
-
-@then('the result page will include "{text}"')
-def step_impl(context, text):
-    if text not in context.response:
-        fail('%r not in %r' % (text, context.response))
