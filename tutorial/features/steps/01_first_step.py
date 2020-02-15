e54e7c56d7bb6e41c8096972939f0df35a208c62
Aga <agnieszkajureczko@o2.pl>
2020-02-15 23:02:51 +0100

    rename, add ref to tutorial

---
+++ tutorial/features/steps/01_first_step.py
@@ -0,0 +1,20 @@
+# Python step implementation for the scenarios
+# https://behave.readthedocs.io/en/latest/tutorial.html#python-step-implementations
+# https://jenisys.github.io/behave.example/tutorials/tutorial01.html
+
+from behave import *
+
+
+@given('we have behave installed')
+def step_impl(context):
+    pass
+
+
+@when('we implement a test')
+def step_impl(context):
+    assert True is not False
+
+
+@then('behave will test it for us!')
+def step_impl(context):
+    assert context.failed is False
