0485f028b6671867bcf30e8bc9899b4433567e7e
Aga <agnieszkajureczko@o2.pl>
2020-02-28 22:19:22 +0100

    remove maximize_window

--- googlesearch/features/environment.py
+++ googlesearch/features/environment.py
@@ -6,12 +6,7 @@ def before_scenario(context, scenario):
     context.driver = webdriver.Chrome("C:/drivers/chromedriver.exe")
     # Wait implicitly for elements to be ready before attempting interactions
     context.driver.implicitly_wait(10)
-    context.driver.maximize_window()
 
 
 def after_scenario(context, scenario):
     context.driver.quit()
-
-
-
-
